48c41e52d0f45c88618d99aa0ded2a510c3a5bee
emmovsixty <emmo1933@gmail.com>
2022-08-05 23:10:39 +0700

    p

---
+++ src/inputOutput.kt
@@ -0,0 +1,8 @@
+fun main(){
+    print("masukan nama anda : ")
+    val nama = readLine()
+    print("umur : ")
+    val umur = readLine()
+    println("nama : $nama")
+    println("umur : $umur")
+}
